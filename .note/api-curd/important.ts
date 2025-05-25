e024973fe80d487ff7a1a10e2e645d0a92b247e4
Toufiquer <toufiquerabc@gmail.com>
2025-05-25 08:27:30 +0600

    done @ health mongoDB

---
+++ .note/api-curd/important.ts
@@ -0,0 +1,4 @@
+/**
+ * ! you can not write others function in route.ts file
+ * ! see example for GET, POST, PUT, DELETE
+ * */
---
+++ .note/mongodb/mongoDB.ts
@@ -0,0 +1,10 @@
+/**
+ * 1. create account
+ * 2. Create free cluster
+ * 3. npm i mongoose
+ * 4. Copy mongoDBURI and save it to.env file
+ * 5. create mongoose.ts file in lib folder [ensure username & password update]
+ * 6. global.d.ts for global variable Type
+ * 7. update Network Access from account. [0.0.0.0]
+ * 8.
+ * **/
